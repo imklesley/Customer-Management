6a3be780f86fb33605224fccf2f19c31696534bc
= <=>
2020-12-06 22:49:54 -0300

    tentativa 1000 pra fazer rodar no heroku

--- CustomerManagement/settings.py
+++ CustomerManagement/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+import django_heroku
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
@@ -194,3 +196,8 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'imklesley@gmail.com'
 # Não deixa essa senha visível não vacilão
 EMAIL_HOST_PASSWORD = 'rsuosrzgydcqotqw'
+
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
